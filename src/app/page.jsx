cbfc98d9ab37ee02db712fe76aefd7e3748dee0d
shubham khodre <shubham.khodre16@gmail.com>
2025-02-18 12:55:23 +0530

    startup  page changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,8 +9,65 @@ import Footer from "../components/footer/Footer";
 import HomeContent from "../components/home/HomeContent";
 import Services from "../components/home/Services";
 import OurCaseStudies from "../components/home/OurCaseStudies";
-
 export default function Home() {
+  const CarouselData = [
+    {
+      title: "Best Ruby on Rails Development Company",
+      backgroundColor: "#650909",
+      backgroundImage: "url(/assets/img/ror-banner.svg)",
+      des: "Get scalable, secure, and well-supported features web applications by embracing exclusive Ruby on rails development services from InfoKodetechnologies. As one of the top specialists in ROR development services.",
+      button: "Read More",
+      path:"/studies/services/schlorship"
+    },
+    {
+      title: "Web Development",
+      backgroundColor: "#340F3D",
+      backgroundImage: "url(/assets/img/web-technology.svg)",
+      des: "For offering enhanced user experience and result-oriented solutions we are having unmat capabilities. A team of experienced web developers at InfoKoders technologies works dedicatedly to produce web solutions that can help to grow your business.",
+      button: "Read More",
+      path:"/studies/services/schlorship"
+    },
+    {
+      title: "We are start-up uplifters",
+      backgroundColor: "#3c004b",
+      backgroundImage: "url(/assets/img/web-technology.svg)",
+      des: "Fulfill your tech and non-tech startup dreams through embracing winning technological solutions from a successful tech team holding up a track record of developing 100+ innovative tech products for start-ups over 5+ years.",
+      button: "Get the Recognition with us",
+       path:"/studies/services/schlorship"
+    },
+    {
+      title: "Let your enterprise rock with our world-class technological services",
+      backgroundColor: "#364F6E",
+      backgroundImage: "url(/assets/img/right.png)",
+      des: "InfoKoders technologies is admired as a leading software Development Company for exceeding business goals consistently thereby offering end-to-end web, mobile, and software development services.",
+      button: "Technologize with us",
+       path:"/studies/services/schlorship"
+    },
+    {
+      title: "Online Scholarship Management Platform",
+      backgroundColor: "#032B44",
+      backgroundImage: "url(/assets/img/schlorshipimage.png)",
+      des: "A Minneapolis-based start-up wished to eradicate the student’s  financial struggle in an educational realm by taking the advantage of new-age technologies. He narrated his entire vision of building an online scholarship management platform for revolutionizing the  process school is paid, helping deserving students, and effective management of award programs.",
+      button: "Read More",
+      path:"/studies/services/schlorship"
+    },
+    {
+      title: "Startup Growth",
+      backgroundColor: "#340F3D",
+      backgroundImage: "url(/assets/img/start-up.svg)",
+      des: "Navigating the tech and Non- tech Start-ups towards success heights with our durable software development solutions. We are here to breathe life into your start-up dreams. InfoKoders Technologies is been recognized as a one-stop source to embrace winning software development solutions at a budget-friendly cost.",
+      button: "Read More",
+       path:"/studies/services/schlorship"
+    },
+    {
+      title: "Agile Developments",
+      backgroundColor: "#032B44",
+      backgroundImage: "url(/assets/img/right2.png)",
+      des: "We follow Agile Development Process, which enables us to deliver the project with the highest quality and a solid product.App development takes time, but by using agile development methodologies, our full-stack web developers make it look easy.",
+      button: "Read More",
+      path:"/studies/services/schlorship"
+    },
+  ];
   return (
     <div>
       <header>
@@ -20,9 +77,8 @@ export default function Home() {
           content="Contact us form for Next.js project"
         />
       </header>
-
       <main className="flex flex-col items-center">
-        <Carousel />
+        <Carousel  data={CarouselData} />
         <HomeContent />
         <OurProcess />
         <Services />
--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -6,106 +6,55 @@ import styles from "../../styles/Carousel.module.css";
 import whiteWave from "../../../public/assets/img/white-wave.png";
 import Link from "next/link";
 import Navbar from "../navbar/Navbar";
-
-const Carousel = () => {
-  const [slideIndex, setSlideIndex] = useState(0);
-
-  const CarouselData = [
-    {
-      title: "Best Ruby on Rails Development Company",
-      backgroundColor: "#650909",
-      backgroundImage: "url(/assets/img/ror-banner.svg)",
-      des: "Get scalable, secure, and well-supported features web applications by embracing exclusive Ruby on rails development services from InfoKodetechnologies. As one of the top specialists in ROR development services.",
-      button: "Read More",
-      path:"/studies/schlorship"
-    },
-    {
-      title: "Web Development",
-      backgroundColor: "#340F3D",
-      backgroundImage: "url(/assets/img/web-technology.svg)",
-      des: "For offering enhanced user experience and result-oriented solutions we are having unmat capabilities. A team of experienced web developers at InfoKoders technologies works dedicatedly to produce web solutions that can help to grow your business.",
-      button: "Read More",
-      path:"/studies/schlorship"
-
-    },
-    {
-      title: "We are start-up uplifters",
-      backgroundColor: "#3c004b",
-      backgroundImage: "url(/assets/img/web-technology.svg)",
-      des: "Fulfill your tech and non-tech startup dreams through embracing winning technological solutions from a successful tech team holding up a track record of developing 100+ innovative tech products for start-ups over 5+ years.",
-      button: "Get the Recognition with us",
-      path:"/studies/schlorship"
-
-    },
-    {
-      title:
-        "Let your enterprise rock with our world-class technological services",
-      backgroundColor: "#364F6E",
-      backgroundImage: "url(/assets/img/right.png)",
-      des: "InfoKoders technologies is admired as a leading software Development Company for exceeding business goals consistently thereby offering end-to-end web, mobile, and software development services.",
-      button: "Technologize with us",
-      path:"/studies/schlorship"
-    },
-    {
-      title: "Online Scholarship Management Platform",
-      backgroundColor: "#032B44",
-      backgroundImage: "url(/assets/img/schlorshipimage.png)",
-      des: "A Minneapolis-based start-up wished to eradicate the student’s  financial struggle in an educational realm by taking the advantage of new-age technologies. He narrated his entire vision of building an online scholarship management platform for revolutionizing the  process school is paid, helping deserving students, and effective management of award programs.",
-      button: "Read More",
-      path:"/studies/schlorship"
-    },
-    {
-      title: "Startup Growth",
-      backgroundColor: "#340F3D",
-      backgroundImage: "url(/assets/img/start-up.svg)",
-      des: "Navigating the tech and Non- tech Start-ups towards success heights with our durable software development solutions. We are here to breathe life into your start-up dreams. InfoKoders Technologies is been recognized as a one-stop source to embrace winning software development solutions at a budget-friendly cost.",
-      button: "Read More",
-      path:"/studies/schlorship"
-    },
-    {
-      title: "Agile Developments",
-      backgroundColor: "#032B44",
-      backgroundImage: "url(/assets/img/right2.png)",
-      des: "We follow Agile Development Process, which enables us to deliver the project with the highest quality and a solid product.App development takes time, but by using agile development methodologies, our full-stack web developers make it look easy.",
-      button: "Read More",
-      path:"/studies/schlorship"
-    },
-  ];
-  const totalSlides = CarouselData.length;
-
-  const nextSlide = () => setSlideIndex((prev) => (prev + 1) % totalSlides);
-  const prevSlide = () =>
-    setSlideIndex((prev) => (prev - 1 + totalSlides) % totalSlides);
-  const goToSlide = (index) => setSlideIndex(index);
-
-  useEffect(() => {
+const Carousel = ({data,isStartup=false}) => {
+const [slideIndex, setSlideIndex] = useState(0);
+const totalSlides = data.length;
+const nextSlide = () => setSlideIndex((prev) => (prev + 1) % totalSlides);
+const prevSlide = () => setSlideIndex((prev) => (prev - 1 + totalSlides) % totalSlides);
+const goToSlide = (index) => setSlideIndex(index);
+useEffect(() => {
     const interval = setInterval(nextSlide, 5000);
     return () => clearInterval(interval);
   }, []);
-
-  return (
-    <div id="carousel" className={styles.slideContainer}>
-      {CarouselData.map((item, index) => (
+ return (
+    <div id="carousel" className={`${styles.slideContainer} ${styles.startupCarousel}`  }>
+      {data.map((item, index) => (
         <Box
           key={index}
-          className={styles.slide}
+          className={`${styles.slide} ${styles.startupSlide}`}
           style={{
             backgroundColor: item.backgroundColor,
             display: slideIndex === index ? "flex" : "none",
+            ...isStartup?{
+              backgroundImage:item.backgroundImage,
+              position:"relative"
+              }:{}
           }}
         >
-          <Grid2 container spacing={2} className={styles.gridContainer}>
-            <Grid2 size={{ xs: 12, md: 7 }} className={styles.textContainer}>
-              <Typography variant="h4" className={styles.title}>
+           {isStartup &&(
+              <Box
+              style={{  position:"absolute",
+                 top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "rgba(0, 0, 0, 0.4)",}}
+                />
+            )
+          }
+          <Grid2 container spacing={2} className={styles.gridContainer} >
+            <Grid2 size={{ xs: 12, md: 7 }} className={`${styles.textContainer} ${styles.startContainer}` } >
+              <Typography variant="h4" className={styles.title}  style={{width:isStartup?'100%' :'auto',color:isStartup?'rgb(183 46 52)':'inherit',opacity:isStartup?0.8:1, fontFamily:'NovemberPro'}}>
                 {item.title}
               </Typography>
+             {!isStartup &&(
               <Image
                 src={whiteWave}
                 alt="White Wave"
                 width={100}
-                height={100}
-              />
-              <Typography variant="body1" className={styles.description}>
+                height={100} 
+              />)}
+              <Typography variant="body1" className={styles.description} style={{opacity:isStartup?0.9:1,fontSize:isStartup?'35px':'22px',fontFamily:isStartup?'NovemberPro':'NovemberPro-Reg' }}>
                 {item.des}
               </Typography>
               <Link href={item?.path} passHref>
@@ -118,25 +67,27 @@ const Carousel = () => {
               </Button>
               </Link>
             </Grid2>
-            <Grid2 size={{ xs: 12, md: 5 }} className={styles.imageContainer}>
+            <Grid2 size={{ xs: 12, md: 5 }} className={styles.imageContainer} style={{display:isStartup?'none':''}}>
               <Box
                 className={styles.backgroundImage}
-                style={{ backgroundImage: item.backgroundImage }}
+                style={{ backgroundImage: item.backgroundImage,
+                  ...isStartup?{
+                    backgroundImage:''
+                  }:{}
+                 }}
               />
             </Grid2>
           </Grid2>
         </Box>
       ))}
-
       <a className={styles.prev} onClick={prevSlide}>
         &#10094;
       </a>
       <a className={styles.next} onClick={nextSlide}>
         &#10095;
       </a>
-
       <div className={styles.dotsContainer}>
-        {CarouselData.map((_, index) => (
+        {data.map((_, index) => (
           <span
             key={index}
             className={`${styles.dot} ${
