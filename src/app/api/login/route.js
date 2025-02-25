d7fbbc72d85ce66d3737635378923209862d5aee
Shubham <shubham.khodre16gmail.com>
2025-02-25 13:19:08 +0530

    user name

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,8 +1,8 @@
 export async function POST(req) {
     const { username, password } = await req.json();
   
-    const storedUsername = process.env.NEXT_PUBLIC_USERNAME;
-    const storedPassword = process.env.NEXT_PUBLIC_PASSWORD;
+    const storedUsername = process.env.USERNAME;
+    const storedPassword = process.env.PASSWORD;
     
     if (username === storedUsername && password === storedPassword) {
       return new Response(JSON.stringify({ success: true }), { status: 200 });
